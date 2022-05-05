5fbdb8c5774678022a62100209281195de5fc6eb
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-04 22:37:06 -0300

    feat: import header component on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Header from 'components/Header'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
@@ -9,14 +10,15 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>rest-countries-api</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
+        <meta name="theme-color" content="#111517" />
         <meta name="description" content="Project from FrontendMentor " />
       </Head>
       <GlobalStyles />
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   )
