ca5fbab6e549603d51e534dc103df00dac422f36
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-05 10:54:56 -0300

    feat: imports components to home page and style it

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -3,23 +3,24 @@ import styled, { css } from 'styled-components'
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     width: 20rem;
-    border: none;
-
     height: 4.8rem;
+
     background-color: ${theme.colors.darkBlue};
-    padding: 1.4rem 2.4rem;
 
     display: flex;
     align-items: center;
     justify-content: center;
+
+    border-radius: ${theme.border.radius.medium};
   `}
 `
 export const Select = styled.select`
   ${({ theme }) => css`
-    font-size: ${theme.font.sizes.small};
+    font-size: ${theme.font.sizes.xsmall};
     color: ${theme.colors.white};
     background-color: ${theme.colors.darkBlue};
-    border-radius: ${theme.border.radius.medium};
+
+    padding-left: 2.5rem;
     padding-right: 6rem;
     border: none;
     outline: none;
--- src/containers/Home/styles.ts
+++ src/containers/Home/styles.ts
@@ -1,3 +1,19 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.darkModeBg};
+  `}
+`
+
+export const FiltersContainer = styled.div`
+  width: 100%;
+  padding: 2rem 1.6rem;
+
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+
+  gap: 4rem;
+`
