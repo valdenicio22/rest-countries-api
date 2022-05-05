bfae981916fc04f3a1b9396c6f4745526cbc0711
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-05 00:09:19 -0300

    add media queries to header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.header`
   ${({ theme }) => css`
@@ -14,14 +15,29 @@ export const HeaderContainer = styled.div`
   width: 100%;
   padding: 0 1.6rem;
 
+  ${media.greaterThan('small')`
+    padding: 0 2.1rem;
+  `}
+  ${media.greaterThan('medium')`
+    padding: 0 3rem;
+  `}
+  ${media.greaterThan('large')`
+    padding: 0 6rem;
+  `}
+
   display: flex;
   align-items: center;
   justify-content: space-between;
 `
 export const HeaderMsg = styled.p`
   ${({ theme }) => css`
-    font-weight: ${theme.font.weight.extraBold};
     line-height: 20px;
+    font-weight: ${theme.font.weight.extraBold};
+    font-size: ${theme.font.sizes.medium};
+
+    ${media.greaterThan('medium')`
+    font-size: ${theme.font.sizes.xxlarge};
+    `}
     color: ${theme.colors.white};
   `}
 `
@@ -44,18 +60,29 @@ export const IconContainer = styled.div`
   ${({ theme }) => css`
     width: 1.5rem;
     height: 1.5rem;
+
+    ${media.greaterThan('medium')`
+      width: 1.8rem;
+      height: 1.8rem;
+    `}
+
     color: ${theme.colors.white};
     font-weight: ${theme.font.weight.bold};
 
     display: flex;
     align-items: center;
     justify-content: center;
+    padding-bottom: 0.3rem;
   `}
 `
 export const CurrentTheme = styled.p`
   ${({ theme }) => css`
     font-weight: ${theme.font.weight.bold};
     font-size: ${theme.font.sizes.xsmall};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.small};
+    `}
     color: ${theme.colors.white};
   `}
 `
