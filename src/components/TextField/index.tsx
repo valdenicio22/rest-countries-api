6bdf002a7ede21ae348f5d228f663f899da9fbea
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-07 19:10:15 -0300

    feat: add filters

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,13 +1,24 @@
 import * as S from './styles'
 import { Search as SearcIcon } from '@styled-icons/bootstrap'
+import { ChangeEventHandler } from 'react'
 
-const TextField = () => (
+type TextFieldProps = {
+  value: string
+  onChange: ChangeEventHandler<HTMLInputElement>
+}
+
+const TextField = ({ value, onChange }: TextFieldProps) => (
   <S.Wrapper>
     <S.InputWrapper>
       <S.Icon>
         <SearcIcon />
       </S.Icon>
-      <S.Input type="text" placeholder="Search for a country..." />
+      <S.Input
+        type="text"
+        placeholder="Search for a country..."
+        value={value}
+        onChange={onChange}
+      />
     </S.InputWrapper>
   </S.Wrapper>
 )
