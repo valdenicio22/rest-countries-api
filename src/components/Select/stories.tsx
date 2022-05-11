323173b8f917177ae803bd792f0f0d7ce1e4f120
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-11 19:09:13 -0300

    feat: remove unnecessary component

--- src/components/Select/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Select, { SelectProps } from '.'
-
-export default {
-  title: 'Select',
-  component: Select
-} as Meta
-
-export const Default: Story<SelectProps> = (args) => <Select {...args} />
