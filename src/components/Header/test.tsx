b78ab56b0d9a8ce5bd7d0f203ff790d05e3d4b53
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-11 11:30:41 -0300

    delete: remove unnecessary file

--- src/components/Header/test.tsx
+++
@@ -1,8 +0,0 @@
-import Header from '.'
-import { renderWithThemeProvider } from '../../Utils/renderWithThemeProvider'
-
-describe('<Header />', () => {
-  it('should render the Header', () => {
-    renderWithThemeProvider(<Header />)
-  })
-})
